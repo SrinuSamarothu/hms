a6b654d1dc807750e1b660fe059fca8034faf294
SrinuSamarothu <srinu143.h@gmail.com>
2023-03-22 17:05:46 +0530

    Api commited

---
+++ ScheduleApi/BusinessLogic/ILogic.cs
@@ -0,0 +1,18 @@
+﻿using FluentApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public interface ILogic
+    {
+        public void AddSchedule(DoctorSchedule doctorSchedule);
+
+        public DoctorSchedule? UpdateSchedule(int day, DoctorSchedule doctorSchedule);
+
+        public IEnumerable<DoctorSchedule>? GetSchedule(string day);
+    }
+}
---
+++ ScheduleApi/FluentApi/Entities/AvailabilityScheduleContext.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FluentApi.Entities;
+
+public partial class AvailabilityScheduleContext : DbContext
+{
+    public AvailabilityScheduleContext()
+    {
+    }
+
+    public AvailabilityScheduleContext(DbContextOptions<AvailabilityScheduleContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DoctorSchedule>(entity =>
+        {
+            entity.HasKey(e => e.DoctorId).HasName("PK__doctor_s__E5AB9877AA5DE297");
+
+            entity.ToTable("doctor_schedule");
+
+            entity.Property(e => e.DoctorId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("Doctor_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
